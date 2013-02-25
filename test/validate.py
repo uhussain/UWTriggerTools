54a5d852f23ccb145c27bad5c84bf73681bc6312
Evan Friis <ekfriis@gmail.com>
2013-02-25 08:43:08 -0600

    Fix bug where only the first event was examined, ignore non-used branches which weren't previously initialized

--- test/validate.py
+++ test/validate.py
@@ -3,9 +3,11 @@
 # Stupid check to ensure the validation files haven't changed
 #
 # Usage:
-# ./validate.py mt_numEvent200.root validation/mt_numEvent200.root rlxTauEfficiency/Ntuple
+# ./validate.py mt_numEvent200.root validation/mt_numEvent200.root \
+        # rlxTauEfficiency/Ntuple
 
 import sys
+import math
 
 file1, file2, ntuple = sys.argv[1], sys.argv[2], sys.argv[3]
 
@@ -22,13 +24,22 @@ f2 = ROOT.TFile(file2)
 ntuple1 = f1.Get(ntuple)
 ntuple2 = f2.Get(ntuple)
 
-branch_names = (x.GetName() for x in ntuple1.GetListOfBranches())
+branch_names = [x.GetName() for x in ntuple1.GetListOfBranches()]
 
 entries = ntuple1.GetEntries()
 
 num_errors = 0
 
 num_above_15 = 0
+
+
+def deltaPhi(x1, x2):
+    diff = x1 - x2
+    while diff > math.pi:
+        diff = diff - 2 * math.pi
+    if diff < -math.pi:
+        diff += math.pi
+    return diff
 
 for i in range(entries):
     ntuple1.GetEntry(i)
@@ -39,9 +50,20 @@ for i in range(entries):
     for branch in branch_names:
         res1 = getattr(ntuple1, branch)
         res2 = getattr(ntuple2, branch)
-        if res1 != res2:
-            print ntuple1.l1gPt
-            print ntuple2.l1gPt
+        #print i, branch, res1, res2
+        delta = abs(res1 - res2)
+        if 'Phi' in branch:
+            delta = deltaPhi(res1, res2)
+        if 'UIC' in branch:
+            delta = 0
+        # we don't care about these branches in the tau files
+        if 'MIP' in branch and 'Tau' in ntuple:
+            delta = 0
+        if 'TauVeto' in branch and 'Tau' in ntuple:
+            delta = 0
+
+        if delta > 1e-5:
+            print ntuple1.l1gPt, ntuple1.l1gEta, ntuple1.l1gPhi
             print "mismatch event %i branch %s (%s %s != %s %s)" % (
                 i, branch, file1, str(res1), file2, str(res2))
             num_errors += 1
