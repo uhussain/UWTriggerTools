a64885103248c91948d2f77624141ee5cff75de6
Evan Friis <ekfriis@gmail.com>
2013-02-28 09:42:06 -0600

    Add additional MC workaround

--- python/emulationMC_cfi.py
+++ python/emulationMC_cfi.py
@@ -14,7 +14,11 @@ Authors: M. Cepeda, T. Perry, E. Friis
 from L1Trigger.UCT2015.emulation_cfi import *  # NOQA
 from L1Trigger.Configuration.ValL1Emulator_cff import *  # NOQA
 
+print "Using workarounds for MC RAW data content bugs"
+
 # We use valHcalTriggerPrimitiveDigis since we emulate these from the full
 # hcalDigis readout.  Otherwise it depends on the presence of the sim digis.
 uctDigiStep += valHcalTriggerPrimitiveDigis
 hackHCALMIPs.src = cms.InputTag("valHcalTriggerPrimitiveDigis")
+# Otherwise everything is zero.
+HcalTPGCoderULUT.LUTGenerationMode = True
