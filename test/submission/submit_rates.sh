013e4acab5ef9900898b88597dc1293cec2ba6e9
Evan Friis <ekfriis@gmail.com>
2013-05-29 14:30:08 -0500

    Ignore EIC threshold

--- test/submission/submit_rates.sh
+++ test/submission/submit_rates.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 # Submit rate ntuple jobs on the ZeroBias3 dataset (2012C)
-EXPECTED_ARGS=2
+EXPECTED_ARGS=1
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: $0 JOB_NAME EICTHRESH"
+  echo "Usage: $0 JOB_NAME"
   exit 1
 fi
 
-farmoutAnalysisJobs $1 \
+farmoutAnalysisJobs $1-HCAL \
   --infer-cmssw-path \
-  --input-file-list=zero_bias_files.txt \
-  ../makeRateTrees_cfg.py  stage1B=1 \
+  --input-file-list=zerobias_14TeV_mc_files.txt \
+  --input-files-per-job=5 \
+  --job-count=100 \
+  ../makeRateTrees_cfg.py  isMC=1 stage1B=1 stage1=0 eicCardHcalOnly=1 \
   'inputFiles=$inputFileNames' 'outputFile=$outputFileName' \
-  eicIsolationThreshold=$2
+  eicIsolationThreshold=3
+
+farmoutAnalysisJobs $1-Norm \
+  --infer-cmssw-path \
+  --input-file-list=zerobias_14TeV_mc_files.txt \
+  --input-files-per-job=5 \
+  --job-count=100 \
+  ../makeRateTrees_cfg.py  isMC=1 stage1B=0 stage1=1 eicCardHcalOnly=0 \
+  'inputFiles=$inputFileNames' 'outputFile=$outputFileName' \
+  eicIsolationThreshold=3
