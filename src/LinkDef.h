6f7444b6fe92973ddf19226eb00f6dc16373d6a9
Evan Friis <ekfriis@gmail.com>
2013-05-31 15:03:20 -0500

    Add dictionaries for custom dataformats

---
+++ src/LinkDef.h
@@ -0,0 +1,8 @@
+#include "L1Trigger/UCT2015/interface/RegionAlgos.h"
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ class RegionDiscriminantInfo;
+#endif
