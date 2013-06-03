a70fdc0eb02784b9d0bbae003e6929a59d9410e8
Evan Friis <ekfriis@gmail.com>
2013-06-03 14:44:53 -0500

    Remove linkdef which never worked

--- src/LinkDef.h
+++
@@ -1,8 +0,0 @@
-#include "L1Trigger/UCT2015/interface/RegionAlgos.h"
-
-#ifdef __CINT__
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-#pragma link C++ class RegionDiscriminantInfo;
-#endif
