3ed39d90fa87a7b4607578b8eeab2ac6eb9dadc6
Laura Dodd <lmdodd@wisc.edu>
2014-02-14 13:09:55 -0600

    Compiling UCTProducer pumult.h

---
+++ interface/pileupmult_corrections.h
@@ -0,0 +1,13 @@
+#ifndef PILEUPMULT_CORRECTIONS
+#define PILEUPMULT_CORRECTIONS
+
+/*
+ * Pile Up Corrections from PileUpMultiplicity0 
+ * The constants are in the cc file.
+ *
+ */
+
+// Version using normal PU subtraction algorithm
+double pumcorr(double pt, int ieta, unsigned int pum0);
+
+#endif
